9c2ced5252393f51250421a04dbf689c1ca108dd
Jayant <jayantjnp@gmail.com>
2017-03-20 15:43:52 +0530

    bug fix

--- resources/views/email/user_invite.blade.php
+++ resources/views/email/user_invite.blade.php
@@ -31,7 +31,7 @@
             <table cellpadding="0" cellspacing="0" border="0">
               <tr>
                 <td>
-                  <a href="{{ URL::to('register/verify/'.$token) }}" style="background-color: #337ab7;border-color: #2e6da4;font-size: 14px;font-weight: 400;line-height: 1.42857143;text-align: center;color:white;text-decoration:none;border: 1px solid transparent;border-radius: 4px;color: #fff;margin-top: 5px;margin-bottom: 5px;padding: 6px 12px;"  target="_blank">Activate</a>
+                  <a href="{{ URL::to('/verifyAccount/'.$token) }}" style="background-color: #337ab7;border-color: #2e6da4;font-size: 14px;font-weight: 400;line-height: 1.42857143;text-align: center;color:white;text-decoration:none;border: 1px solid transparent;border-radius: 4px;color: #fff;margin-top: 5px;margin-bottom: 5px;padding: 6px 12px;"  target="_blank">Activate</a>
                 </td>
               </tr>
             </table>
