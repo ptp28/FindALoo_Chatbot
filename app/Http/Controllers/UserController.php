b8ab99abc32972ae9d869dbde15e29ca826f9932
tusharshahsp <tusharshahsp@gmail.com>
2018-07-23 13:19:21 +0530

    findaloo modified

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -113,7 +113,7 @@ class UserController extends Controller
             'user_name' => 'required|min:5|regex:/(^[A-Za-z. ]+$)+/',
             'user_email' => 'required|email|unique:login,username,'.($request->g_user_id ? ",g_user_id,g_user_id," : ''),
             'g_user_id' => 'required|min:4|unique:login,g_user_id,'.($request->user_email ? ",username,username," : ''),
-            'user_img' => 'required|url|unique:User_Register,image',
+            'user_img' => 'required|url',
             'user_fcm' => 'required'
             // .($request->g_user_id ? ",g_user_id,g_user_id," : '')
         ]);
